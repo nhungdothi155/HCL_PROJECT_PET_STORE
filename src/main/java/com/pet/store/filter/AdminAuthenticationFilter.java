820a2739e20b8a3545144bfe5fbf0bb89d62b264
dothinhung123 <youwillbesucessful@gmail.com>
2021-04-22 23:43:22 +0700

    authenticate for admin page

--- src/main/java/com/pet/store/filter/AdminAuthenticationFilter.java
+++ src/main/java/com/pet/store/filter/AdminAuthenticationFilter.java
@@ -5,21 +5,33 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class AdminAuthenticationFilter implements Filter {
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse =(HttpServletResponse) response;
+		StringBuffer urlPattern = httpRequest.getRequestURL();
+		String url = urlPattern.toString();
+		if(url.contains(".jsp") && !url.equals("error.jsp")) {
+			RequestDispatcher rq = httpRequest.getRequestDispatcher("error.jsp");
+			rq.forward(httpRequest, httpResponse);
+		}
+		chain.doFilter(httpRequest, httpResponse);
 		
 	}
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		
 		// TODO Auto-generated method stub
 		
 	}
